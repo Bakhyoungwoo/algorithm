f0f7d4f3f7579dc3bc558ae4dd3769d55a7e6a32
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2023-11-18 11:37:44 +0900

    Update Heap_sort_min.kt

--- Kotlin/Heap_sort_min.kt
+++ Kotlin/Heap_sort_min.kt
@@ -1,49 +1,50 @@
-fun minHeapify(arr: IntArray, n: Int, i: Int) {
-    var largest = i
-    val leftChild = 2 * i + 1
-    val rightChild = 2 * i + 2
-
-    if (leftChild < n && arr[leftChild] > arr[largest]) {
-        largest = leftChild
-    }
-
-    if (rightChild < n && arr[rightChild] > arr[largest]) {
-        largest = rightChild
-    }
-
-    if (largest != i) {
-        val temp = arr[i]
-        arr[i] = arr[largest]
-        arr[largest] = temp
-
-        minHeapify(arr, n, largest)
-    }
-}
-
-
-fun minHeapSort(arr: IntArray) {
+fun heapSort(arr: IntArray) {
     val n = arr.size
 
-    // Max Heap 만들기
+    // 힙을 구성
     for (i in n / 2 - 1 downTo 0) {
-        minHeapify(arr, n, i)
+        heapify(arr, n, i)
     }
 
-    // 힙에서 요소 하나씩 추출하며 정렬하기
+    // 힙에서 원소를 하나씩 꺼내 정렬
     for (i in n - 1 downTo 1) {
+        // 최소값인 루트 노드와 마지막 노드를 교환
         val temp = arr[0]
         arr[0] = arr[i]
         arr[i] = temp
 
-        minHeapify(arr, i, 0)
+        // 힙을 다시 구성
+        heapify(arr, i, 0)
     }
 }
 
-// 예시 사용법
+fun heapify(arr: IntArray, n: Int, i: Int) {
+    var largest = i
+    val left = 2 * i + 1
+    val right = 2 * i + 2
+
+    // 왼쪽 자식이 부모보다 크면 largest를 왼쪽 자식으로 설정
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left
+    }
+
+    // 오른쪽 자식이 largest보다 크면 largest를 오른쪽 자식으로 설정
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right
+    }
+
+    // largest가 부모가 아니라면 largest와 부모를 교환하고 힙을 다시 구성
+    if (largest != i) {
+        val swap = arr[i]
+        arr[i] = arr[largest]
+        arr[largest] = swap
+        heapify(arr, n, largest)
+    }
+}
+
+
 fun main() {
     val arr = intArrayOf(4, 10, 3, 5, 1)
-
-    println("정렬 전: ${arr.contentToString()}")
-    minHeapSort(arr)
-    println("정렬 후: ${arr.contentToString()}")
+    heapSort(arr)
+    println("정렬된 배열: ${arr.contentToString()}")
 }
