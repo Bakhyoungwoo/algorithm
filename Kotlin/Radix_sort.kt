64657fe5e0194f6dc46287c49fbebf61838d35d4
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2023-11-18 11:51:14 +0900

    Update Radix_sort.kt

--- Kotlin/Radix_sort.kt
+++ Kotlin/Radix_sort.kt
@@ -1,41 +1,41 @@
-//fun countingSort(arr: IntArray, exp: Int) {
-//    val output = IntArray(arr.size)
-//    val count = IntArray(10)
-//
-//    for (i in arr.indices) {
-//        count[(arr[i] / exp) % 10]++
-//    }
-//
-//    for (i in 1 until 10) {
-//        count[i] += count[i - 1]
-//    }
-//
-//    for (i in arr.size - 1 downTo 0) {
-//        output[count[(arr[i] / exp) % 10] - 1] = arr[i]
-//        count[(arr[i] / exp) % 10]--
-//    }
-//
-//    for (i in arr.indices) {
-//        arr[i] = output[i]
-//    }
-//}
-//
-//fun radixSort(arr: IntArray) {// 자릿수 만큼 반복
-//    val max = arr.maxOrNull() ?: return
-//
-//    var exp = 1
-//    while (max / exp > 0) {
-//        countingSort(arr, exp)
-//        exp *= 10
-//    }
-//}
-//
-//fun main() {
-//    print("Enter numbers separated by spaces: ")
-//    val input = readLine() ?: return
-//    val arr = input.split(" ").map { it.toIntOrNull() ?: return }.toIntArray()
-//
-//    println("Original array: ${arr.contentToString()}")
-//    radixSort(arr)
-//    println("Sorted array: ${arr.contentToString()}")
-//}
+fun countingSort(arr: IntArray, exp: Int) {
+   val output = IntArray(arr.size)
+   val count = IntArray(10)
+
+   for (i in arr.indices) {
+       count[(arr[i] / exp) % 10]++
+   }
+
+   for (i in 1 until 10) {
+       count[i] += count[i - 1]
+   }
+
+   for (i in arr.size - 1 downTo 0) {
+       output[count[(arr[i] / exp) % 10] - 1] = arr[i]
+       count[(arr[i] / exp) % 10]--
+   }
+
+   for (i in arr.indices) {
+       arr[i] = output[i]
+   }
+}
+
+fun radixSort(arr: IntArray) {// 자릿수 만큼 반복
+   val max = arr.maxOrNull() ?: return
+
+   var exp = 1
+   while (max / exp > 0) {
+       countingSort(arr, exp)
+       exp *= 10
+   }
+}
+
+fun main() {
+   print("Enter numbers separated by spaces: ")
+   val input = readLine() ?: return
+   val arr = input.split(" ").map { it.toIntOrNull() ?: return }.toIntArray()
+
+   println("정렬 전 : ${arr.contentToString()}")
+   radixSort(arr)
+   println("정렬 후 : ${arr.contentToString()}")
+}
