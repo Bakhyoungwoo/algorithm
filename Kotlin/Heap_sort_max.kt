a042946a24fd991f197692de92544b8755650aa3
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2023-11-18 14:38:00 +0900

    Update Heap_sort_max.kt

--- Kotlin/Heap_sort_max.kt
+++ Kotlin/Heap_sort_max.kt
@@ -1,53 +1,53 @@
 fun maxheapSort(arr: IntArray) {
     val n = arr.size
 
-    //최대 힙 만들기
-    for (i in n / 2 - 1 downTo 0) {
-        maxheapify(arr, n, i)
-    }
+        //최대 힙 만들기
+        for (i in n / 2 - 1 downTo 0) {
+            maxheapify(arr, n, i)
+        }
 
     // 하나씩 추출하여 정렬
     for (i in n - 1 downTo 0) {
         // 현재 최솟값을 배열 끝으로 이동
         val temp = arr[0]
-        arr[0] = arr[i]
-        arr[i] = temp
+            arr[0] = arr[i]
+            arr[i] = temp
 
-        // 반복
-        maxheapify(arr, i, 0)
+            // 반복
+            maxheapify(arr, i, 0)
     }
 }
 
 // 주어진 노드를 루트로 하는 서브트리를 최대 힙으로 만드는 함수
-fun maxheapify(arr: IntArray, n: Int, i: Int) {
-    var smallest = i // 가장 작은 값의 인덱스
-    val left = 2 * i + 1
-    val right = 2 * i + 2
+fun maxheapify(arr: IntArray, n : Int, i : Int) {
+    var largest = i // 가장 작은 값의 인덱스
+        val left = 2 * i + 1
+        val right = 2 * i + 2
 
-    // 왼쪽 자식이 현재 노드보다 작다면
-    if (left < n && arr[left] < arr[smallest]) {
-        smallest = left
-    }
+        // 왼쪽 자식이 현재 노드보다 작다면
+        if (left < n && arr[left] < arr[largest]) {
+            largest = left
+        }
 
     // 오른쪽 자식이 현재 노드보다 작다면
-    if (right < n && arr[right] < arr[smallest]) {
-        smallest = right
+    if (right < n && arr[right] < arr[largest]) {
+        largest = right
     }
 
     // 가장 작은 값이 현재 노드가 아니라면 위치 변경
-    if (smallest != i) {
+    if (largest != i) {
         val swap = arr[i]
-        arr[i] = arr[smallest]
-        arr[smallest] = swap
+            arr[i] = arr[largest]
+            arr[largest] = swap
 
-        // 재귀적으로 서브 트리에 대해 힙 구축
-        maxheapify(arr, n, smallest)
+            // 재귀적으로 서브 트리에 대해 힙 구축
+            maxheapify(arr, n, largest)
     }
 }
 
 fun main() {
     val arr = intArrayOf(12, 11, 13, 5, 6, 7)
-    println("정렬 전: ${arr.contentToString()}")
-    maxheapSort(arr)
-    println("정렬 후: ${arr.contentToString()}")
+        println("정렬 전: ${arr.contentToString()}")
+        maxheapSort(arr)
+        println("정렬 후: ${arr.contentToString()}")
 }
